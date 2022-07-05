b4d206015ef216a641bdc26188d745e56342960d
Doomeath <fernandoompl@gmail.com>
2022-07-04 22:57:38 -0500

    ejercicio 4, tipos de datos

---
+++ ejercicio4.py
@@ -0,0 +1,26 @@
+#Strings letras
+print("Hello world")
+print('Hello world')
+print('''Hello World''')
+print ("""Hello world""")
+#Integer enteros
+print(10)
+#Float decimales
+print(50.1)
+#Boolean activar o desactivar, tipo de dato lógico
+True
+False
+#List se pueden poner diferentes elementos
+[32, 64, 96, 128]
+["Hola", "Adiós", "Hello"]
+[32, "Hola", False, 10.1]
+#Tuples datos que no pueden cambiar
+(32, 64, 96, 128)
+#Dictionaries se definen por una clave y un valor "keys and values"
+{
+    "Pokepuerco":"Jeovani",
+    "Gomax":"Orrala",
+    "Ukki":"Sara"
+}
+#None un tipo de dato que no se ha definido
+None
